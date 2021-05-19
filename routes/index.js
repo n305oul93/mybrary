6b61e225ba5eb6253496df2e2611f90895d849c1
D Pringle <n305oul93@gmail.com>
2021-05-19 06:54:39 -0500

    Books Create/Index/New

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const indexRouter = express.Router()
+const Book = require('../models/book')
 
-indexRouter.get('/', (req, res) => {
-  res.render('index')
+indexRouter.get('/', async (req, res) => {
+  let books
+  try {
+    books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
+  } catch {
+    books = []
+  }
+  res.render('index', { books: books })
 })
 
 module.exports = indexRouter
